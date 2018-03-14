bb8a63baead95f41ac03064386218354187b77f3
miruka <miruka@disroot.org>
2018-03-14 00:16:38 -0400

    Add extra infos as flat keys instead of dict
    tools.replace_keys() can't replace nested keys, too lazy too implemant
    it for now.

--- kana2/info.py
+++ kana2/info.py
@@ -24,26 +24,24 @@ def info(queries, add_extra_info=True):
 
         for post in tools.exec_pybooru_call(CLIENT.post_list, **subquery):
             if add_extra_info:
-                post["kana2"] = extra_info(post)
+                post = extra_info(post)
 
             yield post
 
 
 def extra_info(post):
-    extra = {}
-
-    extra["aspect_ratio"] = utils.get_ratio(post["image_width"],
-                                            post["image_height"])
+    post["kana2_aspect_ratio"] = utils.get_ratio(post["image_width"],
+                                                 post["image_height"])
 
     if post["file_ext"] == "zip":
         # File is ugoira, the webm is to be downloaded instead.
-        extra["ext"] = os.path.splitext(post["large_file_url"])[1][1:]
+        post["kana2_ext"] = os.path.splitext(post["large_file_url"])[1][1:]
     else:
-        extra["ext"] = post["file_ext"]
+        post["kana2_ext"] = post["file_ext"]
 
-    extra["fetch_date"] = arrow.now().format("YYYY-MM-DDTHH:mm:ss.SSSZZ")
+    post["kana2_fetch_date"] = arrow.now().format("YYYY-MM-DDTHH:mm:ss.SSSZZ")
 
-    return extra
+    return post
 
 
 def get_subqueries(queries):
