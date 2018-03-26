9f6719fa0fd01be7ddd5e52d63789328f572c1eb
miruka <miruka@disroot.org>
2018-03-25 23:40:12 -0400

    Return multiple pages as flattened list of posts

--- kana2/info.py
+++ kana2/info.py
@@ -12,22 +12,22 @@ from . import CLIENT, PROCESSES, tools, utils
 
 def pages(queries, add_extra_info=True):
     with ThreadPool(PROCESSES) as pool:
-        return pool.map(one_page, queries)
+        return utils.flatten_list(pool.map(one_page, queries))
 
 
-def one_page(query_, add_extra_info=True):
-    if not isinstance(query_, dict):
-        raise TypeError("Expected one query dictionary, got %s" % type(query_))
+def one_page(query, add_extra_info=True):
+    if not isinstance(query, dict):
+        raise TypeError("Expected one query dictionary, got %s" % type(query))
 
     logging.info("Getting info - tags: %s, page: %s, total: %s, "
                  "limit: %s, posts: %s%s%s",
-                 query_["tags"], query_["page"], query_["total_pages"],
-                 query_["limit"], query_["posts_to_get"],
-                 ", random" if query_["random"] else "",
-                 ", raw"    if query_["raw"]    else "")
+                 query["tags"], query["page"], query["total_pages"],
+                 query["limit"], query["posts_to_get"],
+                 ", random" if query["random"] else "",
+                 ", raw"    if query["raw"]    else "")
 
     return [extra_info(post) if add_extra_info else post
-            for post in tools.exec_pybooru_call(CLIENT.post_list, **query_)]
+            for post in tools.exec_pybooru_call(CLIENT.post_list, **query)]
 
 
 def extra_info(post):
