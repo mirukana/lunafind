6273f0751ed8d7c321fbd596d35dde08ac79a6ea
miruka <miruka@disroot.org>
2018-04-17 00:30:40 -0400

    Only get active notes by default
    The only_active parameter of notes.notes(), set to True by default, will
    filter notes to discard those that are "disabled"/not shown on the main
    post page (e.g. old versions of notes that were fixed).

--- kana2/notes.py
+++ kana2/notes.py
@@ -1,15 +1,20 @@
 """Get notes from booru posts"""
 
-from . import CLIENT, errors, reqwrap
-
-
-def notes(post):
-    if not isinstance(post, dict):
-        raise TypeError("Expected one query dictionary, got %s." % type(post))
-
-    return reqwrap.pybooru_api(CLIENT.note_list, None, post["id"])
+from . import CLIENT, reqwrap
 
 
 def has_notes(post):
+    if not isinstance(post, dict):
+        raise TypeError("Expected one query dictionary, got %s." % type(post))
+
     # last_noted_at can not exist, or be null/false in the JSON.
     return True if post.get("last_noted_at") else False
+
+
+def notes(post, only_active=True):
+    if not isinstance(post, dict):
+        raise TypeError("Expected one query dictionary, got %s." % type(post))
+
+    notes_ = reqwrap.pybooru_api(CLIENT.note_list, post_id=post["id"])
+
+    return [n for n in notes_ if n["is_active"]] if only_active else notes_
