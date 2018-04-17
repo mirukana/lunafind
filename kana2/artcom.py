bac46648de75230f298eb07528c09e497b2f8a9e
miruka <miruka@disroot.org>
2018-04-17 00:41:27 -0400

    Remove pointless artcom try and imports, reqwrap

--- kana2/artcom.py
+++ kana2/artcom.py
@@ -1,19 +1,15 @@
 """Get artist commentary(ies) from booru posts"""
 
-import requests
+from . import CLIENT, reqwrap
 
-from . import CLIENT, errors, reqwrap
+
+def has_artcom(post):
+    return True if post.get("last_commented_at") else False
 
 
 def artcom(post):
     if not isinstance(post, dict):
         raise TypeError("Expected one query dictionary, got %s." % type(post))
 
-    try:
-        return CLIENT.artist_commentary_list(None, post["id"])
-    except KeyError as err:
-        raise errors.CriticalKeyError(post, err.args[0], "cannot get artcom")
-
-
-def has_artcom(post):
-    return True if post.get("last_commented_at") else False
+    return reqwrap.pybooru_api(CLIENT.artist_commentary_list,
+                               post_id=post["id"])
