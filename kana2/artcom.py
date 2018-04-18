498df1dbb1fcc805644cf56a44aa0d1c2acf44fd
miruka <miruka@disroot.org>
2018-04-18 05:50:07 -0400

    Add logging of notes and artcom retrieval

--- kana2/artcom.py
+++ kana2/artcom.py
@@ -1,5 +1,7 @@
 """Get artist commentary from booru posts"""
 
+import logging
+
 import arrow
 
 from . import CLIENT, reqwrap
@@ -9,16 +11,23 @@ def artcom(post):
     if not isinstance(post, dict):
         raise TypeError("Expected one query dictionary, got %s." % type(post))
 
-    # If commentary/commentary_request tag in meta tags, post has an artcom.
-    # This should be true unless the post was created today, in which case
-    # the tagging bot may have not processed them yet.
+    # If post has commentary/commentary_request tag, it should have an artcom.
+    # Trick below is faster than using regex.
+    meta_tags = " %s " % post.get("tag_string_meta")
 
-    meta_tags = " %s " % post.get("tag_string_meta")  # Faster than regex
+    meta_comm = " commentary " in meta_tags or \
+                " commentary_request " in meta_tags
 
-    if " commentary "         in meta_tags or \
-       " commentary_request " in meta_tags or \
-       arrow.get(post["created_at"]) >= arrow.now().shift(hours=-24):
-        # Return [] if a post created today has no artcom
+    # If the post was created in the last 24 hours, the tagging bot may have
+    # not processed them yet to add commentary/commentary_request tags.
+    created_24h = arrow.get(post["created_at"]) >= arrow.now().shift(hours=-24)
+
+    if meta_comm or created_24h:
+        logging.info("Retrieving%s artist commentary for post %s",
+                     " potential" if created_24h and not meta_comm else "",
+                     post.get("id", "without ID"))
+
+        # Will return [] if a post created in the last 24 hours has no artcom.
         return reqwrap.pybooru_api(CLIENT.artist_commentary_list,
                                    post_id=post["id"])
 
--- kana2/notes.py
+++ kana2/notes.py
@@ -1,5 +1,7 @@
 """Get notes from booru posts"""
 
+import logging
+
 from . import CLIENT, reqwrap
 
 
@@ -11,6 +13,8 @@ def notes(post, only_active=True):
     if not post.get("last_noted_at"):
         # Avoid useless request.
         return []
+
+    logging.info("Retrieving notes for post %s", post.get("id", "without ID"))
 
     notes_ = reqwrap.pybooru_api(CLIENT.note_list, post_id=post["id"])
 
