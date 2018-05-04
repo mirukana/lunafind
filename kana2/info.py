790e5e9d837bd0c0d3f3d521876346849700ffc9
miruka <miruka@disroot.org>
2018-05-04 16:53:11 -0400

    Handle booru errors in info.pages()

--- kana2/info.py
+++ kana2/info.py
@@ -7,18 +7,31 @@ import pybooru
 
 from requestspool import RequestsPool
 
-from . import CLIENT, PROCESSES, extra, reqwrap, utils
+from . import CLIENT, PROCESSES, errors, extra, reqwrap, utils
 
 
-def pages(queries, add_extra_info=True):
+def pages(queries, add_extra_info=True, stop_on_err=False):
     with RequestsPool(PROCESSES, pybooru.Danbooru, ("safebooru",)) as pool:
         # starmap() will see a list of (a_query, add_extra_info) tuples,
         # each corresponding to the params for a one_page() function call.
         # >>> list(product([1,2,3], (True,)))
         # [(1, True), (2, True), (3, True)]
         return utils.flatten_list(
-            pool.starmap(one_page, product(queries, (add_extra_info,)))
-        )
+            pool.starmap(one_page_handle_err,
+                         product(queries, (add_extra_info,), (stop_on_err,))))
+
+
+def one_page_handle_err(query, add_extra_info=True, stop_on_err=False,
+                        client=CLIENT):
+    try:
+        return one_page(query, add_extra_info, client)
+    except errors.Kana2Error as err:
+        utils.log_error(err)
+
+        if stop_on_err:
+            raise err
+
+        return []
 
 
 def one_page(query, add_extra_info=True, client=CLIENT):
