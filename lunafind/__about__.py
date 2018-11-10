e53d53a7528d62889aed3be7e2e93d236a4707d5
miruka <miruka@disroot.org>
2018-11-09 20:42:14 -0400

    Update __about__ description

--- lunafind/__about__.py
+++ lunafind/__about__.py
@@ -1,11 +1,11 @@
 # Copyright 2018 miruka
 # This file is part of lunafind, licensed under LGPLv3.
 
-"Python API and CLI tools for Danbooru-based sites."
+"Search, filter and download posts from Danbooru-based sites and local dirs."
 
 __project_name__ = "lunakit"
 __pkg_name__     = "lunafind"
-__version__      = "0.5.0-1"
+__version__      = "0.5.0-2"
 __status__       = "Development"
 # __status__     = "Production"
 
