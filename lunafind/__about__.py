562855dc20c73a1a93892817ffb8e7f245be060b
miruka <miruka@disroot.org>
2018-11-15 02:31:25 -0400

    Bump version to 0.5.1

--- lunafind/__about__.py
+++ lunafind/__about__.py
@@ -5,7 +5,7 @@
 
 __project_name__ = "lunakit"
 __pkg_name__     = "lunafind"
-__version__      = "0.5.0-2"
+__version__      = "0.5.1"
 __status__       = "Development"
 # __status__     = "Production"
 
