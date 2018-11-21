cca83db5f67e5ad877f11cd0359bb2e1317d9d2c
miruka <miruka@disroot.org>
2018-11-20 20:22:20 -0400

    pypi.org stupid description bug

--- lunafind/__about__.py
+++ lunafind/__about__.py
@@ -1,12 +1,11 @@
 # Copyright 2018 miruka
 # This file is part of lunafind, licensed under LGPLv3.
 
-"""Search, filter, inspect, download posts from Danbooru/Safebooru and find
-them back from the command line without setting up any service or database."""
+"Search, filter, inspect, download posts from Danbooru and local folders."
 
 __project_name__ = "lunakit"
 __pkg_name__     = "lunafind"
-__version__      = "0.5.4-1"
+__version__      = "0.5.4-2"
 __status__       = "Development"
 # __status__     = "Production"
 
